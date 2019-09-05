b57740821342331f5fc987c505561cc416dc64f8
shun suzuki <suzuki@hapis.k.u-tokyo.ac.jp>
2019-09-05 10:14:11 +0900

    update

--- client/autdunity/Assets/AUTD/Scripts/Util/GainMap.cs
+++ client/autdunity/Assets/AUTD/Scripts/Util/GainMap.cs
@@ -4,7 +4,7 @@
  * Created Date: 07/10/2018
  * Author: Shun Suzuki
  * -----
- * Last Modified: 04/09/2019
+ * Last Modified: 05/09/2019
  * Modified By: Shun Suzuki (suzuki@hapis.k.u-tokyo.ac.jp)
  * -----
  * Copyright (c) 2019 Hapis Lab. All rights reserved.
@@ -27,33 +27,44 @@ namespace AUTD3Sharp
         {
             get
             {
-                fixed (IntPtr* p = _gains) return p;
+                fixed (IntPtr* p = _gains)
+                {
+                    return p;
+                }
             }
         }
         public unsafe int* IdPointer
         {
             get
             {
-                fixed (int* p = _ids) return p;
+                fixed (int* p = _ids)
+                {
+                    return p;
+                }
             }
         }
 
         public GainMap(params GainPair[] gainPairs)
         {
-            if (gainPairs == null) throw new ArgumentNullException(nameof(gainPairs));
+            if (gainPairs == null)
+            {
+                throw new ArgumentNullException(nameof(gainPairs));
+            }
 
             Size = gainPairs.Length;
             _ids = new int[Size];
             _gains = new IntPtr[Size];
-            for (var i = 0; i < Size; i++)
+            for (int i = 0; i < Size; i++)
             {
                 _ids[i] = gainPairs[i].Id;
                 _gains[i] = gainPairs[i].Gain.GainPtr;
             }
 
-            var duplication = Size > _ids.GroupBy(i => i).Count();
+            bool duplication = Size > _ids.GroupBy(i => i).Count();
             if (duplication)
+            {
                 throw new ArgumentException("Multiple Gains are set for the same Group ID");
+            }
         }
     }
 
@@ -67,17 +78,33 @@ namespace AUTD3Sharp
             Gain = gain;
         }
 
-        public static bool operator ==(GainPair left, GainPair right) => left.Equals(right);
-        public static bool operator !=(GainPair left, GainPair right) => !left.Equals(right);
-        public bool Equals(GainPair other) => Id == other.Id && Gain == other.Gain;
+        public static bool operator ==(GainPair left, GainPair right)
+        {
+            return left.Equals(right);
+        }
+
+        public static bool operator !=(GainPair left, GainPair right)
+        {
+            return !left.Equals(right);
+        }
+
+        public bool Equals(GainPair other)
+        {
+            return Id == other.Id && Gain == other.Gain;
+        }
 
         public override bool Equals(object obj)
         {
             if (obj is GainPair pair)
+            {
                 return Equals(pair);
+            }
+
             return false;
         }
-        public override int GetHashCode() => Id ^ Gain.GetHashCode();
-
+        public override int GetHashCode()
+        {
+            return Id ^ Gain.GetHashCode();
+        }
     }
 }
