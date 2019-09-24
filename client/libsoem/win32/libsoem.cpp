25e57f886595c2eff17298732071ef1bd4ae5abe
shun suzuki <suzuki@hapis.k.u-tokyo.ac.jp>
2019-09-24 16:55:43 +0900

    update: dont work

--- client/libsoem/win32/libsoem.cpp
+++ client/libsoem/win32/libsoem.cpp
@@ -4,19 +4,21 @@
  * Created Date: 23/08/2019
  * Author: Shun Suzuki
  * -----
- * Last Modified: 04/09/2019
+ * Last Modified: 24/09/2019
  * Modified By: Shun Suzuki (suzuki@hapis.k.u-tokyo.ac.jp)
  * -----
  * Copyright (c) 2019 Hapis Lab. All rights reserved.
- * 
+ *
  */
 
 #define __STDC_LIMIT_MACROS
 
 #include <iostream>
+#include <WinError.h>
 #include <cstdint>
 #include <mutex>
 #include <memory>
+#include <queue>
 
 #include "libsoem.hpp"
 #include "ethercat.h"
@@ -33,55 +35,69 @@ public:
 	void Close();
 
 private:
-	void SetSendCheck(bool val);
-	bool GetSendCheck();
+	//void SetSendCheck(bool val);
+	//bool GetSendCheck();
 
 	unique_ptr<uint8_t[]> _IOmap;
+	queue<size_t> _send_size_q;
+	queue<unique_ptr<uint8_t[]>> _send_buf_q;
 	bool _isOpened = false;
 	size_t _devNum = 0;
 	uint32_t _mmResult = 0;
-	bool _sendCheck = false;
-	mutex _mutex;
 	HANDLE _timerQueue = NULL;
 	HANDLE _timer = NULL;
 };
 
 void SOEMController::impl::Send(size_t size, unique_ptr<uint8_t[]> buf)
 {
-	if (_isOpened)
-	{
-		const auto header_size = MOD_SIZE + 4;
-		const auto data_size = TRANS_NUM * 2;
-		const auto includes_gain = ((size - header_size) / data_size) > 0;
-
-		for (size_t i = 0; i < _devNum; i++)
-		{
-			if (includes_gain)
-				memcpy(&_IOmap[OUTPUT_FRAME_SIZE * i], &buf[header_size + data_size * i], TRANS_NUM * 2);
-			memcpy(&_IOmap[OUTPUT_FRAME_SIZE * i + TRANS_NUM * 2], &buf[0], MOD_SIZE + 4);
-		}
-		SetSendCheck(true);
-		while (GetSendCheck())
-			;
-	}
+	_send_size_q.push(size);
+	_send_buf_q.push(move(buf));
 }
 
-void SOEMController::impl::SetSendCheck(bool val)
-{
-	lock_guard<mutex> lock(_mutex);
-	_sendCheck = val;
-}
+// void SOEMController::impl::SetSendCheck(bool val)
+// {
+// 	lock_guard<mutex> lock(_mutex);
+// 	_sendCheck = val;
+// }
 
-bool SOEMController::impl::GetSendCheck()
-{
-	lock_guard<mutex> lock(_mutex);
-	return _sendCheck;
-}
+// bool SOEMController::impl::GetSendCheck()
+// {
+// 	lock_guard<mutex> lock(_mutex);
+// 	return _sendCheck;
+// }
 
 void CALLBACK SOEMController::impl::RTthread(PVOID lpParam, BOOLEAN TimerOrWaitFired)
 {
-	ec_send_processdata();
-	(reinterpret_cast<SOEMController::impl *>(lpParam))->SetSendCheck(false);
+	const auto impl = (reinterpret_cast<SOEMController::impl *>(lpParam));
+	if (impl->_isOpened)
+	{
+		if (impl->_send_buf_q.size() > 0)
+		{
+			const auto size = impl->_send_size_q.front();
+			const auto buf = move(impl->_send_buf_q.front());
+
+			const auto header_size = MOD_SIZE + 4;
+			const auto data_size = TRANS_NUM * 2;
+			const auto includes_gain = ((size - header_size) / data_size) > 0;
+
+			for (size_t i = 0; i < impl->_devNum; i++)
+			{
+				if (includes_gain)
+					memcpy(&impl->_IOmap[OUTPUT_FRAME_SIZE * i], &buf[header_size + data_size * i], TRANS_NUM * 2);
+				memcpy(&impl->_IOmap[OUTPUT_FRAME_SIZE * i + TRANS_NUM * 2], &buf[0], MOD_SIZE + 4);
+			}
+		}
+		ec_send_processdata();
+
+		if (impl->_send_size_q.size() > 0)
+		{
+			impl->_send_size_q.pop();
+			impl->_send_buf_q.pop();
+		}
+	}
+
+	// ec_send_processdata();
+	// (reinterpret_cast<SOEMController::impl *>(lpParam))->SetSendCheck(false);
 }
 
 void SOEMController::impl::Open(const char *ifname, size_t devNum)
@@ -144,8 +160,22 @@ void SOEMController::impl::Close()
 {
 	if (_isOpened)
 	{
+		while (_send_buf_q.size() > 0)
+		{
+			Sleep(1);
+		}
+		Sleep(1);
+
 		if (!DeleteTimerQueueTimer(_timerQueue, _timer, 0))
-			cerr << "DeleteTimerQueue failed." << endl;
+		{
+			if (GetLastError() != ERROR_IO_PENDING)
+				cerr << "DeleteTimerQueue failed." << endl;
+		}
+
+		auto size = (OUTPUT_FRAME_SIZE + INPUT_FRAME_SIZE) * _devNum;
+		vector<uint8_t> null_vec(size,0x00);
+		memcpy(&_IOmap[0], &null_vec[0], size);
+		ec_send_processdata();
 
 		ec_slave[0].state = EC_STATE_INIT;
 		ec_writestate(0);
