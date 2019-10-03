02abffa224dbf311cf115e62fb2e7b434c110479
shun suzuki <suzuki@hapis.k.u-tokyo.ac.jp>
2019-10-03 16:08:12 +0900

    fix

--- client/lib/gain.cpp
+++ client/lib/gain.cpp
@@ -8,13 +8,14 @@
  * Modified By: Shun Suzuki (suzuki@hapis.k.u-tokyo.ac.jp)
  * -----
  * Copyright (c) 2016-2019 Hapis Lab. All rights reserved.
- * 
+ *
  */
 
 #include <iostream>
 #include <string>
 #include <map>
 #include <vector>
+#include <cassert> 
 
 #include "core.hpp"
 #include "autd3.hpp"
@@ -40,15 +41,12 @@ void Gain::build()
 {
 	if (this->built())
 		return;
-	auto geo = this->geometry();
-	if (geo == nullptr)
-	{
-		throw runtime_error("Geometry is required to build Gain");
-	}
+	auto geometry = this->geometry();
+	assert(geometry != nullptr);
 
-	for (int i = 0; i < geo->numDevices(); i++)
+	for (int i = 0; i < geometry->numDevices(); i++)
 	{
-		this->_data[geo->deviceIdForDeviceIdx(i)] = std::vector<uint16_t>(NUM_TRANS_IN_UNIT, 0x0000);
+		this->_data[geometry->deviceIdForDeviceIdx(i)] = std::vector<uint16_t>(NUM_TRANS_IN_UNIT, 0x0000);
 	}
 
 	this->_built = true;
@@ -64,7 +62,7 @@ GeometryPtr Gain::geometry() noexcept
 	return this->_geometry;
 }
 
-void Gain::SetGeometry(const GeometryPtr &geometry) noexcept
+void Gain::SetGeometry(const GeometryPtr& geometry) noexcept
 {
 	this->_geometry = geometry;
 }
@@ -85,23 +83,20 @@ void PlaneWaveGain::build()
 {
 	if (this->built())
 		return;
-	auto geo = this->geometry();
-	if (geo == nullptr)
-	{
-		throw runtime_error("Geometry is required to build Gain");
-	}
+	auto geometry = this->geometry();
+	assert(geometry != nullptr);
 
 	this->_data.clear();
-	const auto ndevice = geo->numDevices();
+	const auto ndevice = geometry->numDevices();
 	for (int i = 0; i < ndevice; i++)
 	{
-		this->_data[geo->deviceIdForDeviceIdx(i)].resize(NUM_TRANS_IN_UNIT);
+		this->_data[geometry->deviceIdForDeviceIdx(i)].resize(NUM_TRANS_IN_UNIT);
 	}
 
-	const auto ntrans = geo->numTransducers();
+	const auto ntrans = geometry->numTransducers();
 	for (int i = 0; i < ntrans; i++)
 	{
-		this->_data[geo->deviceIdForTransIdx(i)].at(i % NUM_TRANS_IN_UNIT) = 0xFF00;
+		this->_data[geometry->deviceIdForTransIdx(i)].at(i % NUM_TRANS_IN_UNIT) = 0xFF00;
 	}
 
 	this->_built = true;
@@ -126,29 +121,27 @@ void FocalPointGain::build()
 	if (this->built())
 		return;
 
-	auto geo = this->geometry();
-	if (geo == nullptr)
-	{
-		throw runtime_error("Geometry is required to build Gain");
-	}
+	auto geometry = this->geometry();
+	assert(geometry != nullptr);
+
 	this->_data.clear();
 
-	const auto ndevice = geo->numDevices();
+	const auto ndevice = geometry->numDevices();
 	for (int i = 0; i < ndevice; i++)
 	{
-		this->_data[geo->deviceIdForDeviceIdx(i)].resize(NUM_TRANS_IN_UNIT);
+		this->_data[geometry->deviceIdForDeviceIdx(i)].resize(NUM_TRANS_IN_UNIT);
 	}
 
-	const auto ntrans = geo->numTransducers();
+	const auto ntrans = geometry->numTransducers();
 	for (int i = 0; i < ntrans; i++)
 	{
-		const auto trp = geo->position(i);
+		const auto trp = geometry->position(i);
 		const auto dist = (trp - this->_point).norm();
 		const auto fphase = fmodf(dist, ULTRASOUND_WAVELENGTH) / ULTRASOUND_WAVELENGTH;
 		const auto phase = static_cast<uint8_t>(round(255.0f * (1.0f - fphase)));
 		uint8_t D, S;
 		SignalDesign(this->_amp, phase, D, S);
-		this->_data[geo->deviceIdForTransIdx(i)].at(i % NUM_TRANS_IN_UNIT) = (static_cast<uint16_t>(D) << 8) + S;
+		this->_data[geometry->deviceIdForTransIdx(i)].at(i % NUM_TRANS_IN_UNIT) = (static_cast<uint16_t>(D) << 8) + S;
 	}
 
 	this->_built = true;
@@ -174,19 +167,16 @@ void BesselBeamGain::build()
 {
 	if (this->built())
 		return;
-	auto geo = this->geometry();
-	if (geo == nullptr)
-	{
-		throw runtime_error("Geometry is required to build Gain");
-	}
+	auto geometry = this->geometry();
+	assert(geometry != nullptr);
 
 	this->_data.clear();
-	const auto ndevice = geo->numDevices();
+	const auto ndevice = geometry->numDevices();
 	for (int i = 0; i < ndevice; i++)
 	{
-		this->_data[geo->deviceIdForDeviceIdx(i)].resize(NUM_TRANS_IN_UNIT);
+		this->_data[geometry->deviceIdForDeviceIdx(i)].resize(NUM_TRANS_IN_UNIT);
 	}
-	const auto ntrans = geo->numTransducers();
+	const auto ntrans = geometry->numTransducers();
 
 	const Vector3f _ez(0.f, 0.f, 1.0f);
 
@@ -198,7 +188,7 @@ void BesselBeamGain::build()
 
 	for (int i = 0; i < ntrans; i++)
 	{
-		const auto trp = geo->position(i);
+		const auto trp = geometry->position(i);
 		const auto _r = trp - this->_point;
 		const Vector3f _v_x_r = _r.cross(_v);
 		const Vector3f _R = cos(_theta_w) * _r + sin(_theta_w) * _v_x_r + _v.dot(_r) * (1.0f - cos(_theta_w)) * _v;
@@ -206,13 +196,13 @@ void BesselBeamGain::build()
 		const auto phase = static_cast<uint8_t>(round(255.0f * (1.0f - fphase)));
 		uint8_t D, S;
 		SignalDesign(this->_amp, phase, D, S);
-		this->_data[geo->deviceIdForTransIdx(i)].at(i % NUM_TRANS_IN_UNIT) = (static_cast<uint16_t>(D) << 8) + S;
+		this->_data[geometry->deviceIdForTransIdx(i)].at(i % NUM_TRANS_IN_UNIT) = (static_cast<uint16_t>(D) << 8) + S;
 	}
 
 	this->_built = true;
 }
 
-GainPtr CustomGain::Create(uint16_t *data, int dataLength)
+GainPtr CustomGain::Create(uint16_t* data, int dataLength)
 {
 	auto gain = CreateHelper<CustomGain>();
 	gain->_rawdata.resize(dataLength);
@@ -226,19 +216,16 @@ void CustomGain::build()
 {
 	if (this->built())
 		return;
-	auto geo = this->geometry();
-	if (geo == nullptr)
-	{
-		throw runtime_error("Geometry is required to build Gain");
-	}
+	auto geometry = this->geometry();
+	assert(geometry != nullptr);
 
 	this->_data.clear();
-	const auto ndevice = geo->numDevices();
+	const auto ndevice = geometry->numDevices();
 	for (int i = 0; i < ndevice; i++)
 	{
-		this->_data[geo->deviceIdForDeviceIdx(i)].resize(NUM_TRANS_IN_UNIT);
+		this->_data[geometry->deviceIdForDeviceIdx(i)].resize(NUM_TRANS_IN_UNIT);
 	}
-	const auto ntrans = geo->numTransducers();
+	const auto ntrans = geometry->numTransducers();
 
 	for (int i = 0; i < ntrans; i++)
 	{
@@ -247,7 +234,7 @@ void CustomGain::build()
 		const auto phase = static_cast<uint8_t>(data);
 		uint8_t D, S;
 		SignalDesign(amp, phase, D, S);
-		this->_data[geo->deviceIdForTransIdx(i)].at(i % NUM_TRANS_IN_UNIT) = (static_cast<uint16_t>(D) << 8) + S;
+		this->_data[geometry->deviceIdForTransIdx(i)].at(i % NUM_TRANS_IN_UNIT) = (static_cast<uint16_t>(D) << 8) + S;
 	}
 
 	this->_built = true;
@@ -266,22 +253,19 @@ void TransducerTestGain::build()
 {
 	if (this->built())
 		return;
-	auto geo = this->geometry();
-	if (geo == nullptr)
-	{
-		throw runtime_error("Geometry is required to build Gain");
-	}
+	auto geometry = this->geometry();
+	assert(geometry != nullptr);
 
 	this->_data.clear();
-	const auto ndevice = geo->numDevices();
+	const auto ndevice = geometry->numDevices();
 	for (int i = 0; i < ndevice; i++)
 	{
-		this->_data[geo->deviceIdForDeviceIdx(i)].resize(NUM_TRANS_IN_UNIT);
+		this->_data[geometry->deviceIdForDeviceIdx(i)].resize(NUM_TRANS_IN_UNIT);
 	}
 
 	uint8_t D, S;
 	SignalDesign(this->_amp, this->_phase, D, S);
-	this->_data[geo->deviceIdForTransIdx(_xdcr_idx)].at(_xdcr_idx % NUM_TRANS_IN_UNIT) = (static_cast<uint16_t>(D) << 8) + S;
+	this->_data[geometry->deviceIdForTransIdx(_xdcr_idx)].at(_xdcr_idx % NUM_TRANS_IN_UNIT) = (static_cast<uint16_t>(D) << 8) + S;
 
 	this->_built = true;
 }
