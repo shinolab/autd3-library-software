68eac61be075f3152f5fcd0f7d7a1eb8d16dd60a
shun suzuki <suzuki@hapis.k.u-tokyo.ac.jp>
2020-02-22 14:57:52 +0900

    fix

--- client/example_soem/simple_soem.cpp
+++ client/example_soem/simple_soem.cpp
@@ -3,15 +3,15 @@
 // Created Date: 24/08/2019
 // Author: Shun Suzuki
 // -----
-// Last Modified: 20/02/2020
+// Last Modified: 22/02/2020
 // Modified By: Shun Suzuki (suzuki@hapis.k.u-tokyo.ac.jp)
 // -----
 // Copyright (c) 2019-2020 Hapis Lab. All rights reserved.
 //
 
-#include "autd3.hpp"
-
 #include <iostream>
+
+#include "autd3.hpp"
 
 using namespace std;
 
@@ -34,20 +34,19 @@ string GetAdapterName() {
 int main() {
   auto autd = autd::Controller::Create();
 
-  //  AddDevice() must be called before Open(), and be called as many times as for the number of AUTDs connected.
+  // AddDevice() must be called before Open(), and be called as many times as for the number of AUTDs connected.
   autd->geometry()->AddDevice(Eigen::Vector3d(0, 0, 0), Eigen::Vector3d(0, 0, 0));
-  //  autd.geometry()->AddDevice(Eigen::Vector3d(0, 0, 0), Eigen::Vector3d(0, 0, 0));
 
   auto ifname = GetAdapterName();
   autd->Open(autd::LinkType::SOEM, ifname);
-  //  If you have already recognized the EtherCAT adapter name, you can write it directly like below.
-  //  autd.Open(autd::LinkType::SOEM, "\\Device\\NPF_{B5B631C6-ED16-4780-9C4C-3941AE8120A6}");
+  // If you have already recognized the EtherCAT adapter name, you can write it directly like below.
+  // autd->Open(autd::LinkType::SOEM, "\\Device\\NPF_{B5B631C6-ED16-4780-9C4C-3941AE8120A6}");
 
   if (!autd->is_open()) return ENXIO;
 
-  //  If you use more than one AUTD, call CalibrateModulation only once after Open.
-  //  It takes several seconds proportional to the number of AUTD you use.
-  //  autd.CalibrateModulation();
+  // If you use more than one AUTD, call CalibrateModulation only once after Open.
+  // It takes several seconds proportional to the number of AUTD you use.
+  // autd->CalibrateModulation();
 
   autd->AppendGainSync(autd::FocalPointGain::Create(Eigen::Vector3d(90, 70, 150)));
   autd->AppendModulationSync(autd::SineModulation::Create(150));  // 150Hz AM
