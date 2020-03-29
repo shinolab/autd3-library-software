5d2c56523190c5102d8c373348a725b3bc51b0fb
shun suzuki <suzuki@hapis.k.u-tokyo.ac.jp>
2020-03-29 16:46:20 +0900

    add: command, MOD_SYNC_BASE_SHIFT

--- client/lib/privdef.hpp
+++ client/lib/privdef.hpp
@@ -3,7 +3,7 @@
 // Created Date: 07/06/2016
 // Author: Seki Inoue
 // -----
-// Last Modified: 21/02/2020
+// Last Modified: 29/03/2020
 // Modified By: Shun Suzuki (suzuki@hapis.k.u-tokyo.ac.jp)
 // -----
 // Copyright (c) 2016-2020 Hapis Lab. All rights reserved.
@@ -37,13 +37,15 @@ enum RxGlobalControlFlags {
   LOOP_BEGIN = 1 << 0,
   LOOP_END = 1 << 1,
   SILENT = 1 << 3,
-  IS_SYNC_FIRST_SYNC0 = 1 << 5,  //  reserved, do not use
+  FORCE_FAN = 1 << 4,
+  IS_SYNC_FIRST_SYNC0 = 1 << 5, //  reserved, do not use
+  MOD_SYNC_BASE_SHIFT = 1 << 6,
 };
 
 struct RxGlobalHeader {
   uint8_t msg_id;
   uint8_t control_flags;
-  int8_t _frequency_shift;  // NO USE
+  uint8_t command;
   uint8_t mod_size;
   uint8_t mod[MOD_FRAME_SIZE];
 };
