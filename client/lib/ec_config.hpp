851e856cf8e9e59b72a0e4a59a1a919dab723c43
shun suzuki <suzuki@hapis.k.u-tokyo.ac.jp>
2020-03-29 16:45:31 +0900

    change: sync0 interval 1ms to 2ms

--- client/lib/ec_config.hpp
+++ client/lib/ec_config.hpp
@@ -3,7 +3,7 @@
 // Created Date: 21/02/2020
 // Author: Shun Suzuki
 // -----
-// Last Modified: 22/02/2020
+// Last Modified: 29/03/2020
 // Modified By: Shun Suzuki (suzuki@hapis.k.u-tokyo.ac.jp)
 // -----
 // Copyright (c) 2020 Hapis Lab. All rights reserved.
@@ -19,7 +19,7 @@ constexpr size_t EC_OUTPUT_FRAME_SIZE = NUM_TRANS_IN_UNIT * 2 + HEADER_SIZE;
 constexpr size_t EC_INPUT_FRAME_SIZE = 2;
 
 constexpr uint32_t EC_SM3_CYCLE_TIME_MICRO_SEC = 1000;
-constexpr uint32_t EC_SYNC0_CYCLE_TIME_MICRO_SEC = 1000;
+constexpr uint32_t EC_SYNC0_CYCLE_TIME_MICRO_SEC = 2000;
 
 constexpr uint32_t EC_SM3_CYCLE_TIME_NANO_SEC = EC_SM3_CYCLE_TIME_MICRO_SEC * 1000;
 constexpr uint32_t EC_SYNC0_CYCLE_TIME_NANO_SEC = EC_SYNC0_CYCLE_TIME_MICRO_SEC * 1000;
