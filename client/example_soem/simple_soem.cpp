37be163c1f3dc016273be34bd8ed07b3c208c60d
shun suzuki <suzuki@hapis.k.u-tokyo.ac.jp>
2020-04-03 11:46:21 +0900

    fix: sample

--- client/example_soem/simple_soem.cpp
+++ client/example_soem/simple_soem.cpp
@@ -3,7 +3,7 @@
 // Created Date: 24/08/2019
 // Author: Shun Suzuki
 // -----
-// Last Modified: 30/03/2020
+// Last Modified: 03/04/2020
 // Modified By: Shun Suzuki (suzuki@hapis.k.u-tokyo.ac.jp)
 // -----
 // Copyright (c) 2019-2020 Hapis Lab. All rights reserved.
@@ -34,33 +34,29 @@ string GetAdapterName() {
 int main() {
   auto autd = autd::Controller::Create();
 
-  // AddDevice() must be called before Open(), and be called as many times as
-  // for the number of AUTDs connected.
-  autd->geometry()->AddDevice(autd::Vector3(0, 0, 0), autd::Vector3(0, 0, 0));
+  // AddDevice() must be called before Open(), and be called as many times as for the number of AUTDs connected.
   autd->geometry()->AddDevice(autd::Vector3(0, 0, 0), autd::Vector3(0, 0, 0));
   // autd->geometry()->AddDevice(autd::Vector3(0, 0, 0), autd::Vector3(0, 0, 0));
 
+  // If you have already recognized the EtherCAT adapter name, you can write it directly like below.
+  // autd->Open(autd::LinkType::SOEM, "\\Device\\NPF_{B5B631C6-ED16-4780-9C4C-3941AE8120A6}");
   auto ifname = GetAdapterName();
   autd->Open(autd::LinkType::SOEM, ifname);
-  // If you have already recognized the EtherCAT adapter name, you can write it
-  // directly like below. autd->Open(autd::LinkType::SOEM, "\\Device\\NPF_{B5B631C6-ED16-4780-9C4C-3941AE8120A6}");
 
   if (!autd->is_open()) return ENXIO;
 
-  // If you use more than one AUTD, call CalibrateModulation only once after
-  // Open. It takes several seconds proportional to the number of AUTD you use.
-  autd->CalibrateModulation();
+  auto firm_info_list = autd->firmware_info_list();
+  for (auto firm_info : firm_info_list) std::cout << firm_info << std::endl;
+
+  // If you use more than one AUTD, call CalibrateModulation() only once after Open().
+  // It takes several seconds and blocks the thread in the meantime.
+  // autd->CalibrateModulation();
+
+  auto m = autd::SineModulation::Create(150);  // 150Hz AM
+  autd->AppendModulationSync(m);
 
   auto g = autd::FocalPointGain::Create(autd::Vector3(90, 70, 150));
   autd->AppendGainSync(g);
-
-  auto m = autd::SineModulation::Create(150);
-  autd->AppendModulationSync(m);  // 150Hz AM
-
-  auto firm_info_list = autd->firmware_info_list();
-  for (auto firm_info : firm_info_list) {
-    std::cout << firm_info << std::endl;
-  }
 
   cout << "press any key to finish..." << endl;
   getchar();
