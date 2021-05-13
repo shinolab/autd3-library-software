826e8630934f82278d2fc39b8d27907d717edd7a
shun suzuki <suzuki@hapis.k.u-tokyo.ac.jp>
2021-05-13 09:00:33 +0900

    rm garbage

--- client/examples/soem.cpp
+++ client/examples/soem.cpp
@@ -3,7 +3,7 @@
 // Created Date: 05/11/2020
 // Author: Shun Suzuki
 // -----
-// Last Modified: 11/05/2021
+// Last Modified: 13/05/2021
 // Modified By: Shun Suzuki (suzuki@hapis.k.u-tokyo.ac.jp)
 // -----
 // Copyright (c) 2021 Hapis Lab. All rights reserved.
@@ -38,12 +38,6 @@ int main() {
     autd::Controller autd;
     autd.geometry()->AddDevice(autd::Vector3(0, 0, 0), autd::Vector3(0, 0, 0));
     // autd.geometry()->AddDevice(autd::Vector3(0, 0, 0), autd::Vector3(0, 0, 0));
-
-    auto x = autd::Vector3(0, 1, 2);
-    auto* p = x.data();
-    std::cout << p[0] << std::endl;
-    std::cout << p[1] << std::endl;
-    std::cout << p[2] << std::endl;
 
     // If you have already recognized the EtherCAT adapter name, you can write it directly like below.
     // auto ifname = "\\Device\\NPF_{B5B631C6-ED16-4780-9C4C-3941AE8120A6}";
