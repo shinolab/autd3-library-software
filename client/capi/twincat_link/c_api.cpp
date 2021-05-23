9db44b48e60ca91adab8232ae6965d4fbede773c
shun suzuki <suzuki@hapis.k.u-tokyo.ac.jp>
2021-05-23 16:12:51 +0900

    fix

--- client/capi/twincat_link/c_api.cpp
+++ client/capi/twincat_link/c_api.cpp
@@ -3,13 +3,14 @@
 // Created Date: 08/03/2021
 // Author: Shun Suzuki
 // -----
-// Last Modified: 19/05/2021
+// Last Modified: 23/05/2021
 // Modified By: Shun Suzuki (suzuki@hapis.k.u-tokyo.ac.jp)
 // -----
 // Copyright (c) 2021 Hapis Lab. All rights reserved.
 //
 
 #include "../base/wrapper_link.hpp"
+#include "./twincat_link.h"
 #include "twincat_link.hpp"
 
 void AUTDTwinCATLink(void** out) {
