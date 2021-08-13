53b45a887ae52dd56066873e503b8ed3df0f185a
shun suzuki <suzuki@hapis.k.u-tokyo.ac.jp>
2021-08-13 12:54:18 +0900

    add tests template

---
+++ client/tests/gain/holo/eigen_backend_test.cpp
@@ -0,0 +1,14 @@
+// File: eigen_backend_test.cpp
+// Project: holo
+// Created Date: 13/08/2021
+// Author: Shun Suzuki
+// -----
+// Last Modified: 13/08/2021
+// Modified By: Shun Suzuki (suzuki@hapis.k.u-tokyo.ac.jp)
+// -----
+// Copyright (c) 2021 Hapis Lab. All rights reserved.
+//
+
+#include <gtest/gtest.h>
+
+TEST(HoloGainEigenBackend, test) {}
