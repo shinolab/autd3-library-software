58e51ccc9b54db077c52044363f3e424651501e7
shun suzuki <suzuki@hapis.k.u-tokyo.ac.jp>
2021-09-11 09:54:39 +0900

    add capi backend

---
+++ client/capi/holo_gain/blas/c_api.cpp
@@ -0,0 +1,19 @@
+// File: c_api.cpp
+// Project: blas
+// Created Date: 11/09/2021
+// Author: Shun Suzuki
+// -----
+// Last Modified: 11/09/2021
+// Modified By: Shun Suzuki (suzuki@hapis.k.u-tokyo.ac.jp)
+// -----
+// Copyright (c) 2021 Hapis Lab. All rights reserved.
+//
+
+#include "../../base/wrapper.hpp"
+#include "./blas_backend.h"
+#include "autd3/gain/blas_backend.hpp"
+
+void AUTDBLASBackend(void** out) {
+  auto* b = BackendCreate(autd::gain::holo::BLASBackend::create());
+  *out = b;
+}
