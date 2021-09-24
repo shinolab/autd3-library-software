e560b841db4d9fc03c363fe3634d3f1d7abb37bb
shun suzuki <suzuki@hapis.k.u-tokyo.ac.jp>
2021-09-24 10:44:45 +0900

    improve readability

--- client/lib/modulation/primitive.cpp
+++ client/lib/modulation/primitive.cpp
@@ -1,9 +1,9 @@
-﻿// File: primitive.cpp
+// File: primitive.cpp
 // Project: modulation
 // Created Date: 14/04/2021
 // Author: Shun Suzuki
 // -----
-// Last Modified: 10/09/2021
+// Last Modified: 24/09/2021
 // Modified By: Shun Suzuki (suzuki@hapis.k.u-tokyo.ac.jp)
 // -----
 // Copyright (c) 2021 Hapis Lab. All rights reserved.
@@ -16,26 +16,26 @@
 #include <cstring>
 #include <numeric>
 
+#include "autd3/core/utils.hpp"
+
 namespace autd::modulation {
 
 ModulationPtr Sine::create(const int freq, const double amp, const double offset) { return std::make_shared<Sine>(freq, amp, offset); }
 
 void Sine::calc() {
-  const auto sf = static_cast<int32_t>(sampling_freq());
+  const auto f_s = static_cast<int32_t>(sampling_freq());
 
-  const auto freq = std::clamp(this->_freq, 1, sf / 2);
+  const auto f = std::clamp(this->_freq, 1, f_s / 2);
 
-  const auto d = std::gcd(sf, freq);
+  const auto k = std::gcd(f_s, f);
 
-  const size_t n = sf / d;
-  const size_t rep = freq / d;
+  const size_t n = f_s / k;
+  const size_t d = f / k;
 
   this->_buffer.resize(n, 0);
   for (size_t i = 0; i < n; i++) {
-    auto amp = this->_amp / 2.0 * std::sin(2.0 * M_PI * static_cast<double>(rep * i) / static_cast<double>(n)) + this->_offset;
-    amp = std::clamp(amp, 0.0, 1.0);
-    const auto duty = std::asin(amp) * 2.0 / M_PI * 255.0;
-    this->_buffer[i] = static_cast<uint8_t>(duty);
+    const auto amp = this->_amp / 2.0 * std::sin(2.0 * M_PI * static_cast<double>(d * i) / static_cast<double>(n)) + this->_offset;
+    this->_buffer[i] = core::Utilities::to_duty(amp);
   }
 }
 
@@ -44,32 +44,30 @@ ModulationPtr SinePressure::create(const int freq, const double amp, const doubl
 }
 
 void SinePressure::calc() {
-  const auto sf = static_cast<int32_t>(sampling_freq());
+  const auto f_s = static_cast<int32_t>(sampling_freq());
 
-  const auto freq = std::clamp(this->_freq, 1, sf / 2);
+  const auto f = std::clamp(this->_freq, 1, f_s / 2);
 
-  const auto d = std::gcd(sf, freq);
+  const auto k = std::gcd(f_s, f);
 
-  const size_t n = sf / d;
-  const size_t rep = freq / d;
+  const size_t n = f_s / k;
+  const size_t d = f / k;
 
   this->_buffer.resize(n, 0);
   for (size_t i = 0; i < n; i++) {
-    auto amp = this->_amp / 2.0 * std::sin(2.0 * M_PI * static_cast<double>(rep * i) / static_cast<double>(n)) + this->_offset;
-    amp = std::clamp(std::sqrt(amp), 0.0, 1.0);
-    const auto duty = std::asin(amp) * 2.0 / M_PI * 255.0;
-    this->_buffer[i] = static_cast<uint8_t>(duty);
+    const auto amp = std::sqrt(this->_amp / 2.0 * std::sin(2.0 * M_PI * static_cast<double>(d * i) / static_cast<double>(n)) + this->_offset);
+    this->_buffer[i] = core::Utilities::to_duty(amp);
   }
 }
 
 ModulationPtr SineLegacy::create(const double freq, const double amp, const double offset) { return std::make_shared<SineLegacy>(freq, amp, offset); }
 
 void SineLegacy::calc() {
-  const auto sf = sampling_freq();
-  const auto freq =
-      std::clamp(this->_freq, static_cast<double>(core::MOD_SAMPLING_FREQ_BASE) / static_cast<double>(core::MOD_SAMPLING_FREQ_DIV_MAX), sf / 2.0);
+  const auto f_s = sampling_freq();
+  const auto f =
+      std::clamp(this->_freq, static_cast<double>(core::MOD_SAMPLING_FREQ_BASE) / static_cast<double>(core::MOD_SAMPLING_FREQ_DIV_MAX), f_s / 2.0);
 
-  const auto t = static_cast<size_t>(std::round(1.0 / freq * sf));
+  const auto t = static_cast<size_t>(std::round(f_s / f));
   this->_buffer.resize(t, 0);
   for (size_t i = 0; i < t; i++) {
     double tamp = 255.0 * _offset + 127.5 * _amp * std::cos(2.0 * M_PI * static_cast<double>(i) / static_cast<double>(t));
