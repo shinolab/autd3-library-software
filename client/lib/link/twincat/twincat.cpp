4a686a639c4a85589bf4514df2000751cd74a563
shun suzuki <suzuki@hapis.k.u-tokyo.ac.jp>
2021-11-21 12:59:19 +0900

    improve twincat link perf

--- client/lib/link/twincat/twincat.cpp
+++ client/lib/link/twincat/twincat.cpp
@@ -31,35 +31,7 @@ struct AmsAddr {
   uint16_t port;
 };
 
-class TwinCATImpl final : public TwinCAT {
- public:
-  TwinCATImpl() : _port(0) {}
-  ~TwinCATImpl() override = default;
-  TwinCATImpl(const TwinCATImpl& v) noexcept = delete;
-  TwinCATImpl& operator=(const TwinCATImpl& obj) = delete;
-  TwinCATImpl(TwinCATImpl&& obj) = delete;
-  TwinCATImpl& operator=(TwinCATImpl&& obj) = delete;
-
-  void open() override;
-  void close() override;
-  void send(const uint8_t* buf, size_t size) override;
-  void receive(uint8_t* rx, size_t buffer_len) override;
-  bool is_open() override;
-
- private:
-  long _port;  // NOLINT
 #ifdef _WIN32
-  AmsNetId _net_id{};
-  HMODULE _lib = nullptr;
-#endif
-};
-
-core::LinkPtr TwinCAT::create() { return std::make_unique<TwinCATImpl>(); }
-
-bool TwinCATImpl::is_open() { return this->_port > 0; }
-
-#ifdef _WIN32
-
 constexpr uint32_t INDEX_GROUP = 0x3040030;
 constexpr uint32_t INDEX_OFFSET_BASE = 0x81000000;
 constexpr uint32_t INDEX_OFFSET_BASE_READ = 0x80000000;
@@ -85,72 +57,102 @@ constexpr auto TCADS_ADS_GET_LOCAL_ADDRESS_EX = "AdsGetLocalAddressEx";
 constexpr auto TCADS_ADS_PORT_CLOSE_EX = "AdsPortCloseEx";
 constexpr auto TCADS_ADS_SYNC_WRITE_REQ_EX = "AdsSyncWriteReqEx";
 constexpr auto TCADS_ADS_SYNC_READ_REQ_EX = "AdsSyncReadReqEx2";
+#endif
+
+class TwinCATImpl final : public TwinCAT {
+ public:
+  TwinCATImpl() : _port(0) {}
+  ~TwinCATImpl() override = default;
+  TwinCATImpl(const TwinCATImpl& v) noexcept = delete;
+  TwinCATImpl& operator=(const TwinCATImpl& obj) = delete;
+  TwinCATImpl(TwinCATImpl&& obj) = delete;
+  TwinCATImpl& operator=(TwinCATImpl&& obj) = delete;
+
+  void open() override;
+  void close() override;
+  void send(const uint8_t* buf, size_t size) override;
+  void receive(uint8_t* rx, size_t buffer_len) override;
+  bool is_open() override;
+
+ private:
+  long _port;  // NOLINT
+#ifdef _WIN32
+  AmsAddr _net_addr{};
+  HMODULE _lib = nullptr;
+
+  TcAdsSyncWriteReqEx _write = nullptr;
+  TcAdsSyncReadReqEx _read = nullptr;
+#endif
+};
+
+core::LinkPtr TwinCAT::create() { return std::make_unique<TwinCATImpl>(); }
+
+bool TwinCATImpl::is_open() { return this->_port > 0; }
+
+#ifdef _WIN32
 
 void TwinCATImpl::open() {
   this->_lib = LoadLibrary("TcAdsDll.dll");
   if (_lib == nullptr) throw core::exception::LinkError("couldn't find TcADS-DLL");
 
-  const auto port_open = reinterpret_cast<TcAdsPortOpenEx>(GetProcAddress(this->_lib, TCADS_ADS_PORT_OPEN_EX));
+  const auto port_open = reinterpret_cast<TcAdsPortOpenEx>(GetProcAddress(this->_lib, TCADS_ADS_PORT_OPEN_EX));  // NOLINT
   this->_port = (*port_open)();
   if (!this->_port) throw core::exception::LinkError("failed to open a new ADS port");
 
   AmsAddr addr{};
-  const auto get_addr = reinterpret_cast<TcAdsGetLocalAddressEx>(GetProcAddress(this->_lib, TCADS_ADS_GET_LOCAL_ADDRESS_EX));
+  const auto get_addr = reinterpret_cast<TcAdsGetLocalAddressEx>(GetProcAddress(this->_lib, TCADS_ADS_GET_LOCAL_ADDRESS_EX));  // NOLINT
   if (const auto ret = get_addr(this->_port, &addr); ret) {
     std::stringstream ss;
     ss << "AdsGetLocalAddress: " << std::hex << ret;
     throw core::exception::LinkError(ss.str());
   }
 
-  this->_net_id = addr.net_id;
+  _write = reinterpret_cast<TcAdsSyncWriteReqEx>(GetProcAddress(this->_lib, TCADS_ADS_SYNC_WRITE_REQ_EX));  // NOLINT
+  _read = reinterpret_cast<TcAdsSyncReadReqEx>(GetProcAddress(this->_lib, TCADS_ADS_SYNC_READ_REQ_EX));     // NOLINT
+
+  this->_net_addr = {addr.net_id, PORT};
 }
 
 void TwinCATImpl::close() {
   if (!this->is_open()) return;
 
-  const auto port_close = reinterpret_cast<TcAdsPortCloseEx>(GetProcAddress(this->_lib, TCADS_ADS_PORT_CLOSE_EX));
-  const auto res = (*port_close)(this->_port);
-  if (res == 0) {
-    this->_port = 0;
-    return;
+  const auto port_close = reinterpret_cast<TcAdsPortCloseEx>(GetProcAddress(this->_lib, TCADS_ADS_PORT_CLOSE_EX));  // NOLINT
+  if (const auto res = (*port_close)(this->_port); res != 0) {
+    std::stringstream ss;
+    ss << "Error on closing (local): " << std::hex << res;
+    throw core::exception::LinkError(ss.str());
   }
-  std::stringstream ss;
-  ss << "Error on closing (local): " << std::hex << res;
-  throw core::exception::LinkError(ss.str());
+
+  this->_port = 0;
 }
 
 void TwinCATImpl::send(const uint8_t* buf, const size_t size) {
   if (!this->is_open()) throw core::exception::LinkError("Link is closed");
 
-  AmsAddr addr = {this->_net_id, PORT};
-  const auto write = reinterpret_cast<TcAdsSyncWriteReqEx>(GetProcAddress(this->_lib, TCADS_ADS_SYNC_WRITE_REQ_EX));
-  const auto ret = write(this->_port,  // NOLINT
-                         &addr, INDEX_GROUP, INDEX_OFFSET_BASE,
-                         static_cast<unsigned long>(size),  // NOLINT
-                         const_cast<void*>(static_cast<const void*>(buf)));
-
-  if (ret == 0) return;
-  // https://infosys.beckhoff.com/english.php?content=../content/1033/tcadscommon/html/tcadscommon_intro.htm&id=
-  // 6 : target port not found
-  std::stringstream ss;
-  ss << "Error on sending data (local): " << std::hex << ret;
-  throw core::exception::LinkError(ss.str());
+  if (const auto ret = this->_write(this->_port,  // NOLINT
+                                    &this->_net_addr, INDEX_GROUP, INDEX_OFFSET_BASE,
+                                    static_cast<unsigned long>(size),  // NOLINT
+                                    const_cast<void*>(static_cast<const void*>(buf)));
+      ret != 0) {
+    // https://infosys.beckhoff.com/english.php?content=../content/1033/tcadscommon/html/tcadscommon_intro.htm&id=
+    // 6 : target port not found
+    std::stringstream ss;
+    ss << "Error on sending data (local): " << std::hex << ret;
+    throw core::exception::LinkError(ss.str());
+  }
 }
 
 void TwinCATImpl::receive(uint8_t* rx, const size_t buffer_len) {
   if (!this->is_open()) throw core::exception::LinkError("Link is closed");
 
-  AmsAddr addr = {this->_net_id, PORT};
-  const auto read = reinterpret_cast<TcAdsSyncReadReqEx>(GetProcAddress(this->_lib, TCADS_ADS_SYNC_READ_REQ_EX));
-
-  unsigned long read_bytes;           // NOLINT
-  const auto ret = read(this->_port,  // NOLINT
-                        &addr, INDEX_GROUP, INDEX_OFFSET_BASE_READ, static_cast<uint32_t>(buffer_len), rx, &read_bytes);
-  if (ret == 0) return;
-
-  std::stringstream ss;
-  ss << "Error on receiving data: " << std::hex << ret;
-  throw core::exception::LinkError(ss.str());
+  unsigned long read_bytes = 0;                  // NOLINT
+  if (const auto ret = this->_read(this->_port,  // NOLINT
+                                   &this->_net_addr, INDEX_GROUP, INDEX_OFFSET_BASE_READ, static_cast<uint32_t>(buffer_len), rx, &read_bytes);
+      ret != 0) {
+    std::stringstream ss;
+    ss << "Error on receiving data: " << std::hex << ret;
+    throw core::exception::LinkError(ss.str());
+  }
 }
 
 #else
